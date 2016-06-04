82b2c690574e63eb8d30853518f29ca1e7813e10
powerboat9 <powerboat9@users.noreply.github.com>
2016-06-04 11:35:25 -0400

    Create core.lua

---
+++ core.lua
@@ -0,0 +1,24 @@
+local fName = ...
+local txt
+do
+    local h = http.get(fName)
+    txt = h.readAll()
+    h.close()
+end
+
+local cursor = {1, 1}
+
+local lines = 0
+local function findLines()
+    _, lines = txt:gsub("\n", "\n")
+end
+
+fondLines()
+
+local function event(...)
+    if args[1] ~= "key" then return nil end
+    local key = args[2]
+    if not args[3] then
+        if (key == keys.up) and (cursor[2] > 1) then
+            cursor[2] = cursor[2] - 1
+        elseif (key == keys.down) and (cursor[2]
